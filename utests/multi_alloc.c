5ca0c6a6672acf91bf7676c8cc9a96d3ea65ef06
jsvapiav <jsvapiav@gmail.com>
2019-10-28 22:31:18 +0300

    Add multi-allocation test

---
+++ utests/multi_alloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "../src/balloc.h"
+
+int main(void) {
+        char *block, *block_prev = (void *) BALLOC_ERROR;
+	int n;
+
+        balloc_init();
+	for (n = 0; n != BALLOC_BLOCK_NUM; ++n) {
+        	block = balloc_alloc();
+		if(block == block_prev)
+			return 1;
+		block_prev = block;
+	}
+
+	for (n = 0; n != BALLOC_BLOCK_NUM; ++n) {
+		;;
+	}
+        return balloc_free(block) && balloc_free(block_prev);
+}
