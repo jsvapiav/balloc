92ce99d3924ee709358ae82a740614606e7d0072
jsvapiav <jsvapiav@gmail.com>
2019-11-06 23:34:31 +0300

    Add pthread_barrier apple implementation. Thread test case is done

--- utests/threads_check.c
+++ utests/threads_check.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "../src/balloc.h"
+#ifdef __APPLE__
+#include "pthread_barrier.h"
+#endif
 
-#define N_THREADS (3)
-#define N_OF_CIRLCES (5)
+#define N_THREADS (10)
+#define N_OF_CIRLCES (15)
 
 typedef struct thread_ags {
 	int id;
-	char *block;
+	char **block;
 } thread_args_t;
 
-static pthread_barrier_t barrier;
+static pthread_barrier_t barrier0, barrier1;
 
 void *thread(void *args) {
 	int n;
-	int status;
+	int status0, status1;
 	int id = ((thread_args_t *) args)->id;
-	char *block = ((thread_args_t *) args)->block;
+	char **block = ((thread_args_t *) args)->block;
 
 	n = 0;
 	while (n < N_OF_CIRLCES) {
+		*block = balloc_alloc();
+		if (*block)
+			*block[0] = id;
 
-		status = pthread_barrier_wait(&barrier);
+		status0 = pthread_barrier_wait(&barrier0);
+		status1 = pthread_barrier_wait(&barrier1);
 		n++;
 	}
-	if (status == PTHREAD_BARRIER_SERIAL_THREAD) {
-		pthread_barrier_destroy(&barrier);
+	if (status0 == PTHREAD_BARRIER_SERIAL_THREAD) {
+		pthread_barrier_destroy(&barrier0);
+	}
+
+	if (status1 == PTHREAD_BARRIER_SERIAL_THREAD) {
+		pthread_barrier_destroy(&barrier1);
 	}
 
 	return NULL;
@@ -37,36 +48,59 @@ int main(void) {
 	pthread_t threads[N_THREADS];
 	thread_args_t args[N_THREADS];
 	int n;
-	int status;
+	int status0, status1;
 
-	if (pthread_barrier_init(&barrier, NULL, N_THREADS + 1))
+	if (pthread_barrier_init(&barrier0, NULL, N_THREADS + 1))
+		return 1;
+
+	if (pthread_barrier_init(&barrier1, NULL, N_THREADS + 1))
 		return 1;
 
 	balloc_init();
 
-	printf("check blocks alloc\n");
-	for (n = 1; n != BALLOC_BLOCK_NUM-1; ++n) {
+	for (n = 3; n != BALLOC_BLOCK_NUM-5; ++n) {
 		blocks[n] = balloc_alloc();
 		if(!blocks[n])
 			return 1;
 	}
 
 	for (n = 0; n != N_THREADS; ++n) {
-		args[n].id = n;
-		args[n].block = t_blocks[n];
+		args[n].id = n + 1;
+		args[n].block = &t_blocks[n];
 		if (pthread_create(&threads[n], NULL, thread, (void *) &args[n]))
 			return 1;
 	}
 
 	n = 0;
 	while (n < N_OF_CIRLCES) {
+		status0 = pthread_barrier_wait(&barrier0);
 
-		status = pthread_barrier_wait(&barrier);
+		int i;
+		int k = 0;
+		for (i = 0; i != N_THREADS; ++i) {
+			if (t_blocks[i] && t_blocks[i][0] == i + 1) {
+				t_blocks[i][0] = 0;
+				balloc_free((void **) &t_blocks[i]);
+				k++;
+			}
+		}
+
+		if (k != (N_THREADS - 2))
+			return 1;
+
+		status1 = pthread_barrier_wait(&barrier1);
+
 		n++;
 	}
-	if (status == PTHREAD_BARRIER_SERIAL_THREAD) {
-		pthread_barrier_destroy(&barrier);
+
+	if (status0 == PTHREAD_BARRIER_SERIAL_THREAD) {
+		pthread_barrier_destroy(&barrier0);
 	}
+	if (status1 == PTHREAD_BARRIER_SERIAL_THREAD) {
+		pthread_barrier_destroy(&barrier0);
+	}
+
+	return 0;
 
 }
 
